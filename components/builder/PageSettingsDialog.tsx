a6b9c68dd52a49f6d3b7e89140024aaece020e17
joshedge0 <joshdedge@gmail.com>
2026-01-23 22:03:19 -0800

    fixed page settings background

--- components/builder/PageSettingsDialog.tsx
+++ components/builder/PageSettingsDialog.tsx
@@ -35,7 +35,7 @@ export default function PageSettingsDialog({ settings, onUpdate, onClose }: Page
   }, [onClose]);
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black/20 flex items-center justify-center z-50">
       <div
         ref={dialogRef}
         className="bg-white rounded-lg shadow-xl w-full max-w-md max-h-[90vh] overflow-y-auto"
